5fcdab9192aaf1317b674b591e5d3a3beae14cad
Xueyang Zhao <674582290@qq.com>
2025-09-29 17:15:41 +0800

    Remove unused repo

--- tha2/mocap/ifacialmocap_pose_converter.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Dict, List
-
-
-class IFacialMocapPoseConverter(ABC):
-    @abstractmethod
-    def convert(self, ifacialmocap_pose: Dict[str, float]) -> List[float]:
-        pass
