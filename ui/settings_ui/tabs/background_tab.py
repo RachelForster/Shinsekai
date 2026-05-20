895a734d66bb289a7dee883a8ad7a57bd8db1db1
RachelForster <674582290@qq.com>
2026-05-21 00:28:07 +0800

    Update link

--- ui/settings_ui/tabs/background_tab.py
+++ ui/settings_ui/tabs/background_tab.py
@@ -104,14 +104,14 @@ class BackgroundSettingsTab(QWidget):
         self._community_bg_btn.setToolTip(tr_i18n("bg.community_btn"))
         self._community_bg_btn.clicked.connect(
             lambda: QDesktopServices.openUrl(
-                QUrl("https://shinsekai.end0rph1n.icu/resources?type=background")
+                QUrl("https://shinsekai.end0rph1n.icu/resources")
             )
         )
         self._upload_bg_btn = QPushButton(tr_i18n("bg.upload_btn"))
         self._upload_bg_btn.setToolTip(tr_i18n("bg.upload_btn"))
         self._upload_bg_btn.clicked.connect(
             lambda: QDesktopServices.openUrl(
-                QUrl("https://wj.qq.com/s2/26616089/b61a/")
+                QUrl("https://shinsekai.end0rph1n.icu/resources")
             )
         )
         community_row.addWidget(self._community_bg_btn)
